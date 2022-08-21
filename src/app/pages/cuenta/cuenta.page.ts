e1a955f466cc6d34dd82b367de6c0d86f5344fb7
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-08-21 20:11:03 -0300

    Pagos con actualizar pedido

--- src/app/pages/cuenta/cuenta.page.ts
+++ src/app/pages/cuenta/cuenta.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Usuario } from 'src/app/interfaces/interfaces';
+import { PedidosService } from 'src/app/services/pedidos.service';
 
 import { UsuarioService } from 'src/app/services/usuario.service';
 
@@ -13,7 +14,7 @@ export class CuentaPage implements OnInit {
 
   usuario: Usuario;
 
-  constructor(private usuarioService: UsuarioService) { }
+  constructor(private usuarioService: UsuarioService, private pedidosService: PedidosService) { }
 
   ngOnInit() {
     this.usuario = this.usuarioService.usuario;
